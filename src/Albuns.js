098474c12cf79c8a898f8ad4a30acf6d8bbfc521
Letícia C Ortiz <leticia.conservani@gmail.com>
2022-08-14 20:40:02 -0300

    Adicionando API Users

--- src/Albuns.js
+++ src/Albuns.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
-const Albuns = () => {
+const Albums = () => {
+    const  [loading, setLoading] = useState(false)
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(response => response.json())
+        .then(data =>{
+            setAlbums(data)
+            setLoading(false)
+        })
+    }, [])
     return(
-        <h1>Álbuns</h1>
+        <>
+            <h1>Álbuns</h1>
+            {
+                loading === true ? 'Carregando...' : ''
+            }
+            {
+                albums.map(album =>{
+                    return(
+                        <div>
+                            <span>{album.title} - id: {album.id}</span>
+                        </div>
+                    )
+                })
+            } 
+        </>
     )
 }
 
-export default Albuns
+export default Albums
--- src/Users.js
+++ src/Users.js
@@ -1,8 +1,39 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 
 const Users = () => {
+    const  [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data =>{
+            setUsers(data)
+            setLoading(false)
+        })
+    }, [])
     return(
-        <h1>Users</h1>
+        <>
+            <h1>Users</h1>
+            {
+                loading === true ? 'Carregando...' : ''
+            }
+            {
+                users.map(user => {
+                    return(
+                        <div style={{margin:'10px'}}>
+                            <b>Nome:</b> {user.name}<br/> 
+                            <b>Username:</b> {user.username}<br/>       
+                            <b>Email:</b> {user.email}<br/>
+                            <b>Telefone:</b> {user.phone}<br/>
+                            <b>Site</b> {user.website}<br/>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
 
