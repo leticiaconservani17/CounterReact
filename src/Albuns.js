59ad2f52b2c5f9195fb3f4cbdb4247556525c5e6
Letícia C Ortiz <leticia.conservani@gmail.com>
2022-08-14 18:37:11 -0300

    Adicionando navegação

---
+++ src/Albuns.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Albuns = () => {
+    return(
+        <h1>Álbuns</h1>
+    )
+}
+
+export default Albuns
--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Albuns from './Albuns'
 import Counter from './Counter'
+import Users from './Users'
+
+const pages = {
+  albuns: {
+    text: 'Álbuns',
+    component : Albuns,
+  },
+  counter: {
+    text: 'Counter',
+    component: Counter,
+  },
+  users: {
+    text: 'Usuários',
+    component: Users,
+  },
+}
+
 
 function App(){
+  const [page, setPage] = useState('albuns')
+
+  const handleChangePage = page => {
+    setPage(page)
+  }
+
+  const Page = pages[page].component
+
+  const namePages = Object.keys(pages)
+
   return(
     <>
-    <Counter/>
+    {
+      namePages.map(page => <button onClick={()=> handleChangePage(page)}>{pages[page].text}</button>)
+    }
+    { Page && <Page/>}
     </>
   )
 }
